742223195a6463296e61cde593ecd53d5de51a30
ronniej <ronniej@naver.com>
2026-07-30 09:56:14 +0900

    복사 생성자(얕은 복사) 진행

---
+++ CopyConstructor/Main.cpp
@@ -0,0 +1,13 @@
+﻿#include "Player.h"
+#include <iostream>
+
+int main()
+{
+	Player* player1 = new Player();
+	Player* player2 = new Player("Ronnie");
+	Player* player3 = new Player(*player2);
+
+	delete player1;
+	delete player2;
+	delete player3;
+}
