f90cd367505415b929db095484c9e88723105cb0
ronniej <ronniej@naver.com>
2026-08-04 09:57:28 +0900

    커스텀 RTTI 내용 진행중
    - CraftObject 기본 클래스 선언 부분까지 완료
    - 매크로는 추가 구현해야함.

---
+++ CustomRTTI/CraftObject.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <memory>		// 스마트 포인터 사용을 위해.
+
+// 커스텀 RTTI를 제공하는 기반 클래스.
+class CraftObject
+{
+public:
+	// 현재 객체의 타입 아이디 반환 함수.
+	// 순수 가상 함수(Pure Virtual Function).
+	// 이 클래스를 상속하는 클래스는 필수로 구현해야 함(강제 효과).
+	virtual size_t GetType() const = 0;
+
+	// 타입 비교 함수.
+	// CraftObject의 함수를 호출했다는 건 타입이 다르다는 것과 같음.
+	virtual bool Is(size_t id) const
+	{
+		return false;
+	}
+
+	// 타입 질의 함수.
+	template<typename T>
+	bool IsTypeOf() const
+	{
+		return Is(T::TypeId());
+	}
+};
+
+// 스마트 포인트 형변환 유틸 함수.
+template<typename T, typename U>
+std::shared_ptr<T> Cast(const std::shared_ptr<U>& object)
+{
+	// null 확인.
+	if (!object)
+	{
+		return nullptr;
+	}
+
+	// object의 실제 타입이 T(또는 T의 하위) 타입인지 확인 후 형변환.
+	// 재귀적으로 처리.
+	if (object->Is(T::TypeId()))
+	{
+		return std::static_pointer_cast<T>(object);
+	}
+
+	// 형변환하려는 타입이 아니면 null 반환.
+	return nullptr;
+}
---
+++ CustomRTTI/Main.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+#define TO_NAME(x) #x
+#define CLASS_NAME(type)					\
+	public:									\
+	virtual const char* ClassName() const	\
+	{										\
+		return TO_NAME(type);				\
+	}
+
+class Actor
+{
+	CLASS_NAME(Actor)
+
+public:
+	static size_t TypeId()
+	{
+		// 정적 지역 변수.
+		static int runTimeTypeId = 0;
+		return reinterpret_cast<size_t>(&runTimeTypeId);
+	}
+};
+
+class Player : public Actor
+{
+	CLASS_NAME(Player)
+
+public:
+	static size_t TypeId()
+	{
+		// 정적 지역 변수.
+		static int runTimeTypeId = 0;
+		return reinterpret_cast<size_t>(&runTimeTypeId);
+	}
+};
+
+int main()
+{
+	Actor* actor = new Actor();
+	Actor* player = new Player();
+
+	std::cout << actor->ClassName() << "\n";
+	std::cout << player->ClassName() << "\n";
+
+	// 원시 문자열 비교.
+	// strcmp 구현해보기.
+	//if (strcmp(actor->ClassName(), player->ClassName()) == 0)
+	//{
+	//	std::cout << "두 타입이 같음.\n";
+	//}
+	//else
+	//{
+	//	std::cout << "두 타입이 다름.\n";
+	//}
+
+	// 타입 ID로 비교.
+	if (actor->TypeId() == Player::TypeId())
+	{
+		std::cout << "두 타입이 같음.\n";
+	}
+	else
+	{
+		std::cout << "두 타입이 다름.\n";
+	}
+
+	delete actor;
+	actor = nullptr;
+
+	delete player;
+	player = nullptr;
+}
